35380b78a76d9beff47a020f5cb7ba6ae8cd5d90
Kha-Lik <kolyavaskevich19@gmail.com>
2019-03-23 23:04:49 +0200

    Completed 3rd var task

--- BP_LAB_4/Form1.Designer.cs
+++ BP_LAB_4/Form1.Designer.cs
@@ -66,11 +66,11 @@
             this.tabPage1.Controls.Add(this.richTextViewFile);
             this.tabPage1.Controls.Add(this.toolStrip1);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Name = "FirstTask";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage1.Size = new System.Drawing.Size(793, 429);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.Text = "First task";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // label2
@@ -146,6 +146,7 @@
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(59, 22);
             this.btnClose.Text = "Close file";
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // btnCalc
             // 
@@ -155,6 +156,7 @@
             this.btnCalc.Name = "btnCalc";
             this.btnCalc.Size = new System.Drawing.Size(60, 22);
             this.btnCalc.Text = "Calculate";
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
             // 
             // btnExit
             // 
@@ -164,15 +166,16 @@
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(29, 22);
             this.btnExit.Text = "Exit";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // tabPage2
             // 
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Name = "SecTask";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(793, 429);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "Second task";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // Form1
--- BP_LAB_4/Form1.cs
+++ BP_LAB_4/Form1.cs
@@ -15,31 +15,60 @@ namespace BP_LAB_4
     {
 
         static string path = "C:\\Users\\MykolaV\\source\\repos\\C#\\BP_LABS\\BP_LAB_4\\f.txt";
-        public Form1()
-        {
-            InitializeComponent();
-        }
+        static IEnumerable<double> numbers;
+
+        public Form1() => InitializeComponent();
 
         private void btnOpen_Click(object sender, EventArgs e)
         {
-            FileInfo info = new FileInfo(path);
-            using (FileStream f = new FileStream(path, FileMode.Open))
+            try
             {
-                using (StreamReader fReader = new StreamReader(f))
-                {            
+                richTextViewFile.Text = File.ReadAllText(path, Encoding.UTF8);
+                IEnumerable<string> textEnumerable = File.ReadLines(path);
 
-                    if (info.Exists)
-                    {
-                        while (fReader.EndOfStream)
-                        {
-                            richTextViewFile.AppendText(fReader.ReadLine());
-                        }
-                    } else
-                    {
-                        MessageBox.Show("File error", "File does not exists", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    }
-                }
+                numbers = from i in textEnumerable
+                          let number = Convert.ToDouble(i)
+                          select number;
+
+
+            } catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double max = numbers.Max();
+                LinkedList<double> evens = new LinkedList<double>();
+                for (int i = 0; i < numbers.Count(); i++)
+                {
+                    if(i%2==0) evens.AddLast(numbers.ElementAt(i));
+                }
+                double minEven = evens.Min();
+
+                textMax.Text = Convert.ToString(max);
+                textMinEven.Text = Convert.ToString(minEven);
+            } 
+            catch (Exception ex)
+            {
+                MessageBox.Show("File not opened", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            richTextViewFile.Text = "";
+            textMax.Text = "";
+            textMinEven.Text ="";
         }
     }
 }
