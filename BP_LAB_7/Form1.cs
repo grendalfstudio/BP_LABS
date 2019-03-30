ccc79860a4c0a3cf2f72dc1a36af81d2b36e54f5
AndrryArmor <andrry1@gmail.com>
2019-03-30 04:48:13 +0200

    Added two classes according to the variants

--- BP_LAB_7/Form1.cs
+++ BP_LAB_7/Form1.cs
@@ -17,4 +17,75 @@ namespace BP_LAB_7
             InitializeComponent();
         }
     }
+
+    namespace Variant1
+    {
+        public class City
+        {
+            public string Name { get; }
+            public List<Street> Streets { get; }
+            public City(string name)
+            {
+                Name = name;
+            }
+        }
+        public class Street
+        {
+            public string Name { get; }
+            public List<House> Houses { get; }
+            public Street(string name)
+            {
+                Name = name;
+            }
+        }
+        public class House
+        {
+            public Point Coordinates { get; }
+            public Street Street { get; }
+            public House(Street street, Point coordinates)
+            {
+                Street = street;
+                Coordinates = coordinates;
+            }
+    }
+    
+    namespace Variant3
+    {
+        public class Firm
+        {
+            public string Name { get; }
+            public List<Employee> Employees { get; }
+            public List<Office> Offices { get; }
+            public Firm(string name)
+            {
+                Name = name;
+            }
+        }
+        public class Employee
+        {
+            public string Name { get; }
+            public string Surname { get; }
+            public Firm Firm { get; }
+            public List<Office> Offices { get; }
+            public Employee(string name, string surname, Firm firm)
+            {
+                Name = name;
+                Surname = surname;
+                Firm = firm;
+            }
+            public bool IsAccessable(Office office)
+            {
+                return Firm.Offices.Contains(office);
+            }
+        }
+        public class Office
+        {
+            public string Name { get; }
+            public List<Employee> Employees { get; }
+            public Office(string name)
+            {
+                Name = name;
+            }
+        }
+    }
 }
