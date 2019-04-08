6138eebec9d06e66f34bf48c3627cb1bbc3c411a
Kha-Lik <kolyavaskevich19@gmail.com>
2019-04-08 23:28:02 +0300

    Added 9th lab

---
+++ BP_LAB_9/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BP_LAB_9
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ BP_LAB_9/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace BP_LAB_9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
