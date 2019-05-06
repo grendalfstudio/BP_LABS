74671c0835eff828cba237a66d713f1fc9ca0067
AndrryArmor <andrry1@gmail.com>
2019-05-06 21:44:32 +0300

    Minor changes

--- BP_LAB_12/FormMain.cs
+++ BP_LAB_12/FormMain.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 namespace BP_LAB_12
 {
@@ -14,6 +17,8 @@ namespace BP_LAB_12
     {
         private Timer timer;
         private Graphics g;
+        Trapezoid trapezoid1;
+        Trapezoid trapezoid2;
         public FormMain()
         {
             InitializeComponent();
@@ -23,29 +28,59 @@ namespace BP_LAB_12
             timer.Tick += Timer_Tick;
         }
 
-        private void DrawTrapezoid(int x, int y, float angle)
-        {
-            const int R = 50;
-            PointF[] points = new PointF[4];
-            Pen pen = new Pen(Color.Red, 2);
-            points[1] = new PointF(x, y);
-            points[2] = new PointF(x + R, y);
-            points[3] = new PointF(points[2].X + (float)Math.Cos(angle) * R, points[2].Y + (float)Math.Sin(angle) * R);
-            points[0] = new PointF(points[1].X - (float)Math.Cos(angle) * R, points[1].Y + (float)Math.Sin(angle) * R);
-            g.DrawPolygon(pen, points);
-        }
-        
         private void ButtonStart_Click(object sender, EventArgs e)
         {
+            /// Initialization
             float firstAngle = float.Parse(textBoxFirstTrapezoid.Text);
             float secondAngle = float.Parse(textBoxSecondTrapezoid.Text);
-            DrawTrapezoid(50, 150, firstAngle);
-            DrawTrapezoid(500, 150, secondAngle);
+            trapezoid1 = new Trapezoid(g, 50, 150, firstAngle);
+            trapezoid2 = new Trapezoid(g, 500, 150, secondAngle);
+
+            /// Serializing the trapezoids
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream1 = new FileStream("Trapezoid1.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+            formatter.Serialize(stream1, trapezoid1);
+            stream1.Close();
+            Stream stream2 = new FileStream("Trapezoid2.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+            formatter.Serialize(stream2, trapezoid2);
+            stream1.Close();
+
+            ///Main actions
+            trapezoid1.Draw();
+            trapezoid2.Draw();
             timer.Start();
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
+            trapezoid1.Move();
+            trapezoid2.Move();
+        }
+    }
+    [Serializable]
+    class Trapezoid
+    {
+        private const int R = 50;
+        private Graphics g;
+        private PointF[] points;
+        private readonly Pen pen;
+        public Trapezoid(Graphics graphics, int x, int y, float angle)
+        {
+            g = graphics;
+            pen = new Pen(Color.Red, 2);
+            points = new PointF[4];
+            points[1] = new PointF(x, y);
+            points[2] = new PointF(x + R, y);
+            points[3] = new PointF(points[2].X + (float)Math.Cos(angle) * R, points[2].Y + (float)Math.Sin(angle) * R);
+            points[0] = new PointF(points[1].X - (float)Math.Cos(angle) * R, points[1].Y + (float)Math.Sin(angle) * R);
+        }
+        public void Draw()
+        {
+            g.DrawPolygon(pen, points);
+        }
+        public void Move()
+        {
+            g.Clear(Color.Red);
 
         }
     }
